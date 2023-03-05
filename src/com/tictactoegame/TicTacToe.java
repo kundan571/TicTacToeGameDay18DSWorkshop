030ff754aab4a0b3fa18471262e2b09ac106ecc8
kundan kumar <kundanchauhan1997@gmail.com>
2023-03-05 14:51:16 +0530

    Added: user have to input letter X or O and Placed a mark for user

--- src/com/tictactoegame/TicTacToe.java
+++ src/com/tictactoegame/TicTacToe.java
@@ -1,5 +1,7 @@
 package com.tictactoegame;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
     char[][] board;
@@ -28,10 +30,20 @@ public class TicTacToe {
             System.out.println("-------------");
         }
     }
+    void placeMark(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter row and columnth position where you want to place a mark:!!");
+        int row = scanner.nextInt();
+        int column = scanner.nextInt();
+        System.out.println("Choose a letter X Or O to place a mark");
+        char mark = scanner.next().charAt(0);
+        board[row][column] = mark;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe Game:!!");
         TicTacToe tacToe = new TicTacToe();
         tacToe.displayBoard();
+        tacToe.placeMark();
     }
 }
